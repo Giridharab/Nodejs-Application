4d9ab6ed4da423dbee49d0f72b240470c99df67a
Giridhara B <bgiridhara28@gmail.com>
2024-10-28 18:46:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,175 @@
-# main.tf
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
-  instance_type = "t2.micro"
+resource "aws_vpc" "nodejs-app_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "nodejs-app-vpc"
+  }
 }
 
+resource "aws_subnet" "nodejs-app_subnet" {
+  count = 2
+  vpc_id                  = aws_vpc.nodejs-app_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.nodejs-app_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "nodejs-app-subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "nodejs-app_igw" {
+  vpc_id = aws_vpc.nodejs-app_vpc.id
+
+  tags = {
+    Name = "nodejs-app-igw"
+  }
+}
+
+resource "aws_route_table" "nodejs-app_route_table" {
+  vpc_id = aws_vpc.nodejs-app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nodejs-app_igw.id
+  }
+
+  tags = {
+    Name = "nodejs-app-route-table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count          = 2
+  subnet_id      = aws_subnet.nodejs-app_subnet[count.index].id
+  route_table_id = aws_route_table.nodejs-app_route_table.id
+}
+
+resource "aws_security_group" "nodejs-app_cluster_sg" {
+  vpc_id = aws_vpc.nodejs-app_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nodejs-app-cluster-sg"
+  }
+}
+
+resource "aws_security_group" "nodejs-app_node_sg" {
+  vpc_id = aws_vpc.nodejs-app_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nodejs-app-node-sg"
+  }
+}
+
+resource "aws_eks_cluster" "nodejs-app" {
+  name     = "nodejs-app-cluster"
+  role_arn = aws_iam_role.nodejs-app_cluster_role.arn
+
+  vpc_config {
+    subnet_ids         = aws_subnet.nodejs-app_subnet[*].id
+    security_group_ids = [aws_security_group.nodejs-app_cluster_sg.id]
+  }
+}
+
+resource "aws_eks_node_group" "nodejs-app" {
+  cluster_name    = aws_eks_cluster.nodejs-app.name
+  node_group_name = "nodejs-app-node-group"
+  node_role_arn   = aws_iam_role.nodejs-app_node_group_role.arn
+  subnet_ids      = aws_subnet.nodejs-app_subnet[*].id
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
+  }
+
+  instance_types = ["t2.large"]
+
+  remote_access {
+    ec2_ssh_key = var.ssh_key_name
+    source_security_group_ids = [aws_security_group.nodejs-app_node_sg.id]
+  }
+}
+
+resource "aws_iam_role" "nodejs-app_cluster_role" {
+  name = "nodejs-app-cluster-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "nodejs-app_cluster_role_policy" {
+  role       = aws_iam_role.nodejs-app_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "nodejs-app_node_group_role" {
+  name = "nodejs-app-node-group-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "nodejs-app_node_group_role_policy" {
+  role       = aws_iam_role.nodejs-app_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "nodejs-app_node_group_cni_policy" {
+  role       = aws_iam_role.nodejs-app_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "nodejs-app_node_group_registry_policy" {
+  role       = aws_iam_role.nodejs-app_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
